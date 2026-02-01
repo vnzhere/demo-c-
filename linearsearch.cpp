87c9d689abf594368fe35e0ae9ba0645f4e913c2
Vanzhere <vaniya.sharma158@gmail.com>
2026-02-01 12:38:13 +0000

    Code

---
+++ linearsearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int array[], int size, int searchValue)
+{
+    for (int i=0; i< size; i++)
+    {
+        if (searchValue == array[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main (){
+
+    int a[] = {15, 23, 7, 25, 8, 7 ,16};
+    
+    int userValue;
+
+    cout << "Enter an integer: " << endl;
+    cin >> userValue;
+
+    int result = linearSearch(a, 7, userValue);
+
+    if(result >= 0)
+    {
+        cout << "The number" << a[result] << "Was found at the element with Index" << result << endl;
+
+    }
+    else
+    {
+        cout << "The Number " << userValue << " Was not found." << endl;
+
+    }
+}
